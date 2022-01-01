854bf16982ad6cf5457128fe09e62971aca83497
SAKURAI Hitohiro <sakurai@tokyo-ict.com>
2022-01-01 18:57:09 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ if [[ ! $RULE_CHOICE -eq 1 && ! $RULE_CHOICE -eq 2 ]]; then
 fi
 
 echo "Preparing ..."
-/usr/bin/docker pull mataelang/snorqttalpine-sensor:latest
+/usr/bin/docker pull mataelang/snorqttalpine-sensor:stable
 
 echo "Configuring ..."
 mkdir -p /etc/mataelang-sensor
@@ -48,7 +48,7 @@ cp ${SCRIPTPATH}/service/mataelang-snort.service /etc/systemd/system/
 
 if [[ $RULE_CHOICE -eq 1 ]]; then
   echo "Using Snort Community Rules.."
-  docker tag mataelang/snorqttalpine-sensor:latest mataelang-snort
+  docker tag mataelang/snorqttalpine-sensor:stable mataelang-snort
 fi
 
 if [[ $RULE_CHOICE -eq 2 ]]; then
