8362f154f475b18ae5d8f48d94b0edfef14e1bac
Fadhil Yori Hibatullah <fadhil4812@gmail.com>
2022-02-18 18:00:56 +0700

    add volume paramater for snort_parser for logging

--- update-rule.sh
+++ update-rule.sh
@@ -8,6 +8,8 @@ fi
 command -v docker >/dev/null 2>&1 || { echo >&2 "This service requires Docker, but your computer doesn't have it. Install Docker then try again. Aborting."; exit 1; }
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+IMAGE_TAG=dev
+IMAGE_NAME=mataelang/snorqttsensor-stable
 
 echo "Shutting down Mata Elang Snort Sensor"
 systemctl stop mataelang-snort.service
@@ -25,20 +27,20 @@ echo "Removing the old container and image ..."
 /usr/bin/docker image rm mataelang-snort
 
 echo "Preparing ..."
-/usr/bin/docker pull mataelang/snorqttsensor-stable:1.0
+/usr/bin/docker pull ${IMAGE_NAME}:${IMAGE_TAG}
 
 echo "Building the Docker Image..."
 if [[ $RULE_CHOICE -eq 1 ]]; then
   echo "Using Snort Community Rules.."
-  docker tag mataelang/snorqttsensor-stable:1.0 mataelang-snort
+  docker tag ${IMAGE_NAME}:${IMAGE_TAG} mataelang-snort
 fi
 
 if [[ $RULE_CHOICE -eq 2 ]]; then
   read -p "Input your oinkcode here : " OINKCODE
-  /usr/bin/docker build --no-cache --build-arg OINKCODE=${OINKCODE} -f ${SCRIPTPATH}/dockerfiles/snort.dockerfile -t mataelang-snort ${SCRIPTPATH}/
+  /usr/bin/docker build --no-cache --build-arg IMAGE_TAG=${IMAGE_TAG} --build-arg OINKCODE=${OINKCODE} -f ${SCRIPTPATH}/dockerfiles/snort.dockerfile -t mataelang-snort ${SCRIPTPATH}/
 fi
 
 echo "Re-creating container..."
-/usr/bin/docker create --name mataelang-sensor --network host -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone --env-file /etc/mataelang-sensor/sensor.env mataelang-snort
+/usr/bin/docker create --name mataelang-sensor --network host -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone -v /var/log/mataelang-sensor:/var/log/mataelang-sensor --env-file /etc/mataelang-sensor/sensor.env mataelang-snort
 echo "Starting sensor..."
 systemctl start mataelang-snort.service
